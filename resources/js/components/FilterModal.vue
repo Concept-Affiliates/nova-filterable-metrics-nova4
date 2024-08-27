c6a120b1401a270f119cb4f142958ddd97fb5344
Scott Martens <smartens80@gmail.com>
2024-08-27 15:13:37 +1000

    Revert "Trying to avoid the auto creation of the div with class "@container/modal relative mx-auto z-20 max-w-xl modal" - need to remove the modal class from this div"
    This reverts commit e81cda08b86f5ddc4e150e275b70d7d2f4a7706c.

--- resources/js/components/FilterModal.vue
+++ resources/js/components/FilterModal.vue
@@ -1,105 +1,103 @@
 <template>
-  <Modal :show="openModal" class="modal"  tabindex="-1" role="dialog">
-    <div class="@container/modal relative mx-auto z-20 max-w-xl">
-      <div class="bg-white rounded-lg shadow-lg overflow-hidden w-action-fields">
-          <ModalHeader>
-              Filter: {{ title }}
-          </ModalHeader>
-          <ModalContent>
-            <div
-              v-if="ranges && ranges.length > 0"
-              class="flex border-b border-40"
-            >
-              <div class="w-1/5 px-8 py-6">
-                <label
-                  for="nova-time-range"
-                  class="inline-block text-80 pt-2 leading-tight"
-                  >Time Range</label
+  <Modal :show="openModal" class=""  tabindex="-1" role="dialog">
+    <div class="bg-white rounded-lg shadow-lg overflow-hidden w-action-fields">
+        <ModalHeader>
+            Filter: {{ title }}
+        </ModalHeader>
+        <ModalContent>
+          <div
+            v-if="ranges && ranges.length > 0"
+            class="flex border-b border-40"
+          >
+            <div class="w-1/5 px-8 py-6">
+              <label
+                for="nova-time-range"
+                class="inline-block text-80 pt-2 leading-tight"
+                >Time Range</label
+              >
+            </div>
+            <div class="py-6 px-8 w-1/2">
+              <select
+                id="nova-time-range"
+                v-if="ranges.length > 0"
+                @change="handleChange(null, $event)"
+                class="w-full form-control form-select"
+              >
+                <option
+                  v-for="option in ranges"
+                  :key="option.value"
+                  :value="option.value"
+                  :selected="selectedRangeKey == option.value"
                 >
-              </div>
-              <div class="py-6 px-8 w-1/2">
+                  {{ option.label }}
+                </option>
+              </select>
+            </div>
+          </div>
+          <div
+            v-for="filter in filters"
+            :key="filter.value"
+            class="flex"
+          >
+            <div class="w-1/5 pr-0 py-3">
+              <label
+                :for="filter.name"
+                class="inline-block text-80 pt-2 leading-tight"
+                >{{ filter.name }}</label
+              >
+            </div>
+            <div class="py-3 px-8 w-4/5">
+                <input
+                    class="w-full flex form-control form-control-sm form-input form-input-bordered"
+                    type="date"
+                    ref="dateTimePicker"
+                    v-if="filter.component.includes('date-filter')"
+                    :dusk="`${filter.name}-date-filter`"
+                    name="date-filter"
+                    autocomplete="off"
+                    :value="selectedFilters[filter.class] || filter.currentValue"
+                    :placeholder="placeholder"
+                    @change="handleChange(filter, $event)"
+                />
+                <input
+                    :id="filter.name"
+                    :class="inputClasses(filter.component)"
+                    v-else-if="filter.component !== 'select-filter'"
+                    @change="handleChange(filter, $event)"
+                    :type="filter.component"
+                    :checked="selectedFilters[filter.class] === 1"
+                    :value="selectedFilters[filter.class]"
+                />
                 <select
-                  id="nova-time-range"
-                  v-if="ranges.length > 0"
-                  @change="handleChange(null, $event)"
-                  class="w-full form-control form-select"
+                    :id="filter.name"
+                    v-if="filter.component === 'select-filter'"
+                    @change="handleChange(filter, $event)"
+                    class="w-full form-control form-select form-select-bordered"
+                    style="appearance: auto;"
                 >
-                  <option
-                    v-for="option in ranges"
-                    :key="option.value"
-                    :value="option.value"
-                    :selected="selectedRangeKey == option.value"
-                  >
-                    {{ option.label }}
-                  </option>
+                    <option value selected v-if="!filter.currentValue && filter.currentValue !== 0">{{ __('Choose') }}</option>
+                    <option
+                      v-for="option in filter.options"
+                      :key="option.value"
+                      :value="option.value"
+                      :selected="option.value == selectedFilters[filter.class]"
+                    >
+                      {{ option.label }}
+                    </option>
                 </select>
-              </div>
             </div>
-            <div
-              v-for="filter in filters"
-              :key="filter.value"
-              class="flex"
-            >
-              <div class="w-1/5 pr-0 py-3">
-                <label
-                  :for="filter.name"
-                  class="inline-block text-80 pt-2 leading-tight"
-                  >{{ filter.name }}</label
+          </div>
+        </ModalContent>
+        <ModalFooter>
+            <div class="ml-auto">
+                <DefaultButton
+                    @click.prevent="closeModal"
+                    type="submit"
                 >
-              </div>
-              <div class="py-3 px-8 w-4/5">
-                  <input
-                      class="w-full flex form-control form-control-sm form-input form-input-bordered"
-                      type="date"
-                      ref="dateTimePicker"
-                      v-if="filter.component.includes('date-filter')"
-                      :dusk="`${filter.name}-date-filter`"
-                      name="date-filter"
-                      autocomplete="off"
-                      :value="selectedFilters[filter.class] || filter.currentValue"
-                      :placeholder="placeholder"
-                      @change="handleChange(filter, $event)"
-                  />
-                  <input
-                      :id="filter.name"
-                      :class="inputClasses(filter.component)"
-                      v-else-if="filter.component !== 'select-filter'"
-                      @change="handleChange(filter, $event)"
-                      :type="filter.component"
-                      :checked="selectedFilters[filter.class] === 1"
-                      :value="selectedFilters[filter.class]"
-                  />
-                  <select
-                      :id="filter.name"
-                      v-if="filter.component === 'select-filter'"
-                      @change="handleChange(filter, $event)"
-                      class="w-full form-control form-select form-select-bordered"
-                      style="appearance: auto;"
-                  >
-                      <option value selected v-if="!filter.currentValue && filter.currentValue !== 0">{{ __('Choose') }}</option>
-                      <option
-                        v-for="option in filter.options"
-                        :key="option.value"
-                        :value="option.value"
-                        :selected="option.value == selectedFilters[filter.class]"
-                      >
-                        {{ option.label }}
-                      </option>
-                  </select>
-              </div>
+                    {{ __('Close') }}
+                </DefaultButton>
             </div>
-          </ModalContent>
-          <ModalFooter>
-              <div class="ml-auto">
-                  <DefaultButton
-                      @click.prevent="closeModal"
-                      type="submit"
-                  >
-                      {{ __('Close') }}
-                  </DefaultButton>
-              </div>
-          </ModalFooter>
-      </div>
+        </ModalFooter>
     </div>
   </Modal>
 </template>
